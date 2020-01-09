701e850c83de931a8342c3e60da1794d5ccd7e4d
Tonika Valderrama <tokich6@gmail.com>
2020-01-08 21:16:27 -0500

    refactor toggle function and fix github url issue

--- portfolio.js
+++ portfolio.js
@@ -1,39 +1,31 @@
 
-
-function myFunction(x) {
-  x.classList.toggle("change");
-}
-
 function classToggle() {
-    const navs = document.querySelectorAll('.item')
-    navs.forEach(nav => nav.classList.toggle('toggleShow'));
-  }
-  document.querySelector('.link-toggle').addEventListener('click', classToggle);
-
-  
-function changeColor (y) {
-  const menu = document.querySelector('.main-nav')
+  const navs = document.querySelectorAll('.item');
+  const menu = document.querySelector('.main-nav');
+  const bar = document.querySelector('.bar_container');
+  navs.forEach(nav => nav.classList.toggle('toggleShow'));
   menu.classList.toggle("navStyle");
-     
-} 
+  bar.classList.toggle('change');
+}
+document.querySelector('.link-toggle').addEventListener('click', classToggle);
 
-document.querySelector('.link-toggle').addEventListener('click', changeColor);
 
-// window.onclick = function(event) {
-//   if(!event.target.matches('.link-toggle')) {
-//     var dropdowns = document.getElementsByClassName("item");  
-//                 var i; 
-//                 for (i = 0; i < dropdowns.length; i++) { 
-//                     var openDropdown = dropdowns[i]; 
-//                     if (openDropdown.classList.contains('toggleShow')) { 
-//                         openDropdown.classList.remove('toggleShow'); 
-//                     }
+
+// document.addEventListener ('mouseup', function (event) {
+//   const links = document.getElementsByClassName("item");
+//   const menu = document.querySelector('.main-nav');
+//   const bar = document.querySelector('.bar_container');
+//   let i;
+//   for (i = 0; i < links.length; i++) {
+//     let link = links[i];
+//     console.log(link);
+//     if (!event.target.matches('.link-toggle')) {
+//       if (link.classList.contains('toggleShow') && menu.classList.contains('navStyle') && bar.classList.contains('change')) {
+//         link.classList.remove('toggleShow');
+//         menu.classList.remove('navStyle');
+//         bar.classList.remove('change');
+//       }
 //     }
-//   } 
-//  }
+//   }
+// });
 
-
-
-   
-
-    
